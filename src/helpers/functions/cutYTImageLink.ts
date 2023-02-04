cf8e4313b1614a4e84365306c4dcfb0aa829763f
Evgeny Nikiforov <enikiforov@devexperts.com>
2023-02-05 02:14:16 +0300

    fix for yt images protocol

--- src/helpers/functions/cutYTImageLink.ts
+++ src/helpers/functions/cutYTImageLink.ts
@@ -1,7 +1,16 @@
 import { YTImage } from "src/Types";
 
 export const cutYTImageLink = (img: YTImage): YTImage => {
-    const addr = img.url.slice(0, 2) === "//" ? `http:${img.url}` : img.url;
+    let addr: string;
+    if (img.url.slice(0, 2) === "//") {
+        const url = new URL(`http:${img.url}`);
+        if (url.host === "yt3.googleusercontent.com") {
+            url.protocol = "https:";
+        }
+        addr = url.toString();
+    } else {
+        addr = img.url;
+    }
     return {
         ...img,
         url: addr,
